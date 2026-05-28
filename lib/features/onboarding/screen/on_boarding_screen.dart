a1c6b0ddd4bc0295b9c87a672ebedb5f0bdcbacb
Ahmed-Elmekawy <elmekawy281@gmail.com>
2026-05-28 16:04:11 +0300

    fix: fix issues

--- lib/features/onboarding/screen/on_boarding_screen.dart
+++ lib/features/onboarding/screen/on_boarding_screen.dart
@@ -66,7 +66,6 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
     final currentPageData = _pages[_currentIndex];
 
     return Scaffold(
